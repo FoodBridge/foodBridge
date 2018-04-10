af9d8ebb4ea74e69b5002066049697ee20d3279a
cdrbbt <iosdev@ety-idev-mba-03.local>
2018-04-10 13:37:47 +0300

    Initial commit

---
+++ Food Bridge/Food Bridge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Bridge
+//
+//  Created by iosdev on 10.4.2018.
+//  Copyright © 2018 FoodBridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
