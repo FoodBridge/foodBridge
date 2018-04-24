476e6012ec63a89363dc9a28243a38ea3ca5e6b5
anttinym <nyman.antti@gmail.com>
2018-04-24 11:55:42 +0300

    prototype for food item in fridge view

---
+++ Food Bridge/Food Bridge/FoodItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodItemTableViewCell.swift
+//  Food Bridge
+//
+//  Created by iosdev on 24.4.2018.
+//  Copyright © 2018 FoodBridge. All rights reserved.
+//
+
+import UIKit
+
+class FoodItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
